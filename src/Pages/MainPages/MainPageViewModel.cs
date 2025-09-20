3a038415d0ce04a383b342ef1ef38664bb9aba82
Демидов Денис Михайлович <denis.demidov@cybersteel.com>
2025-09-20 22:48:26 +0500

    [VAM-2]

--- src/Pages/MainPages/MainPageViewModel.cs
+++ src/Pages/MainPages/MainPageViewModel.cs
@@ -4,12 +4,4 @@ using CommunityToolkit.Mvvm.Input;
 namespace VampireTheMasquerade.Pages.MainPages;
 public partial class MainPageViewModel : ObservableObject
 {
-
-	#region Commands
-	[RelayCommand]
-	private void TopMenuButton()
-	{
-
-	} 
-	#endregion
 }
